b9355e74a080bf1ca7a8775222a36aa3c075996a
zinen <zinen@users.noreply.github.com>
2020-12-11 21:58:49 +0100

    Update doLogbuyScrape.js

--- modules/doLogbuyScrape.js
+++ modules/doLogbuyScrape.js
@@ -89,10 +89,10 @@ async function doLogbuyScrape (browserHolder, masterData = null) {
     return scrapeData
   }
 
-  async function testDataFile () {// eslint-disable-line
-    const fileContent = await my.readFile('./20190905-rabat-scrape/data/2019-12-24T161927 - all error.json')
-    return JSON.parse(fileContent)
-  }
+//   async function testDataFile () {// eslint-disable-line
+//     const fileContent = await my.readFile('./20190905-rabat-scrape/data/2019-12-24T161927 - all error.json')
+//     return JSON.parse(fileContent)
+//   }
 
   function testDataConst () {// eslint-disable-line
     const testData = [
