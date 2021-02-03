72fe2a65aee53b3d02b5ca7df5098b2e9d07d32b
zinen <zinen@users.noreply.github.com>
2021-02-03 09:51:12 +0100

    fix: typo

--- test/confirmJsonDist.test.js
+++ test/confirmJsonDist.test.js
@@ -12,7 +12,7 @@ async function run () {
       let result
       try { result = JSON.parse(fileContent) } catch { isValidJSON = false }
       assert.ok(isValidJSON, `Expected content of ${file} to be parse-able as JSON`)
-      assert.notStrictEqual(result.length.length, 0, `Expected length of ${file} to be more then 0, result was ${result.length.length}`)
+      assert.notStrictEqual(result.length, 0, `Expected length of ${file} to be more then 0, result was ${result.length}`)
       console.log(`\x1b[32m✓\x1b[0m Content of ${file} was found ok`)
     }
     console.log('\x1b[32m✓\x1b[0m All tests completed successfully. Files inside ./dist is parsable as JSON')
