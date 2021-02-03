90bfda2ebbc62d434cb36dca3d61d459b6868ed9
zinen <zinen@users.noreply.github.com>
2021-02-03 09:46:16 +0100

    More output info

--- test/confirmJsonDist.test.js
+++ test/confirmJsonDist.test.js
@@ -5,14 +5,15 @@ const assert = require('assert').strict
 async function run () {
   try {
     const dirContent = await myFunc.readDir('./dist')
-    assert.notStrictEqual(dirContent.length, 0)
+    assert.notStrictEqual(dirContent.length, 0, `Expected content of ./dist to more than 0 files, result was ${dirContent.length}`)
+    console.log('\x1b[32m✓\x1b[0m File content of ./dist is more than 0')
     for (const file of dirContent) {
       const fileContent = await myFunc.readFile(file)
       let isValidJSON = true
       let result
       try { result = JSON.parse(fileContent) } catch { isValidJSON = false }
       assert.ok(isValidJSON, `Expected content of ${file} to be parse-able as JSON`)
-      assert.notStrictEqual(result.length, 0, `Expected length of ${file} to be more then 0, result was ${result.length}`)
+      assert.notStrictEqual(result.length, 0, `Expected length of ${file} to be more than 0, result was ${result.length}`)
       console.log(`\x1b[32m✓\x1b[0m Content of ${file} was found ok`)
     }
     console.log('\x1b[32m✓\x1b[0m All tests completed successfully. Files inside ./dist is parsable as JSON')
